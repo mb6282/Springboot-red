5ef965a39164ac4db0c51b33884964cb03e58dfe
mb6282 <marbea6282@gmail.com>
2022-09-04 16:07:26 +0900

    영속화를 통한 update기능 구현

--- src/main/java/site/metacoding/red/domain/users/Users.java
+++ src/main/java/site/metacoding/red/domain/users/Users.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import lombok.Getter;
 import lombok.Setter;
+import site.metacoding.red.web.dto.reqest.users.UpdateDto;
 
 @Setter
 @Getter
@@ -13,5 +14,16 @@ public class Users {
 	private String password;
 	private String email;
 	private Timestamp createdAt;
+	
+	public void 전체수정(UpdateDto updateDto) {
+		//영속화가 되어 있으므로 수정할 body값만 받으면 됨
+		this.username=updateDto.getUsername();
+		this.password=updateDto.getPassword();
+		this.email=updateDto.getEmail();
+	}
+	
+	public void 패스워드수정(String password) {
+		this.password=password;
+	}
 
 }
--- src/main/java/site/metacoding/red/web/UsersController.java
+++ src/main/java/site/metacoding/red/web/UsersController.java
@@ -41,15 +41,50 @@ public class UsersController {
 		return new RespDto<>(1, "회원가입완료", null);
 	}
 	
+//	@PutMapping("users/{id}")
+//	public RespDto<?> update(@PathVariable Integer id, UpdateDto updateDto){
+//		Users users = new Users();
+//		users.setId(id);
+//		users.setUsername(updateDto.getUsername());
+//		users.setPassword(updateDto.getPassword());
+//		users.setEmail(updateDto.getEmail());
+//		usersDao.update(users);
+//		return new RespDto<>(1,"회원수정완료",null);
+//	}
+	
 	@PutMapping("users/{id}")
 	public RespDto<?> update(@PathVariable Integer id, UpdateDto updateDto){
-		Users users = new Users();
-		users.setId(id);
-		users.setUsername(updateDto.getUsername());
-		users.setPassword(updateDto.getPassword());
-		users.setEmail(updateDto.getEmail());
-		usersDao.update(users);
+		//1번 : id로 select 하자. (영속화 퍼시스턴스)
+		//DB에 있는 하나의 row를 java object로 옮기는 것을 영속화라고 함
+		Users usersPS = usersDao.findById(id);
+		
+		//2번 : 변경 (User Entity에 업데이트 책임 수만큼 메서드를 만들어두고 적용)
+		//=의미있는 메서드(수행해야 할 업데이트만을 만든다)를 만듬 
+		usersPS.전체수정(updateDto);
+		
+		//3번 : 영속화된 오브젝트로 update하기 (전체 업데이트)
+		//3번은 영속화되어 모든 값이 있으므로 전체 업데이트가 가능하다.
+		usersDao.update(usersPS);
+		
+		//통신으로 Users를 받지 않고 메서드에도 set으로 값을 받지 않으므로
+		//이 메서드의 경우 Users에 setter가 없어도 됨
+		
 		return new RespDto<>(1,"회원수정완료",null);
+	}
+	
+	@PutMapping("users/{id}/password")
+	public RespDto<?> updatePassword(@PathVariable Integer id, String password){
+		//1번 : 영속화
+		Users usersPS = usersDao.findById(id);
+		
+		//2번 : 변경(Users에서 메서드 만들기)
+		usersPS.패스워드수정(password);
+		//사실은 이런 1번, 2번 코드가 Service에 있어야 함
+		
+		//3번 : 영속화된 오브젝트로 update하기 (전체 업데이트)
+		usersDao.update(usersPS);
+
+		return new RespDto<>(1,"회원패스워드수정완료",null);
 	}
 	
 	@DeleteMapping("/users/{id}")
